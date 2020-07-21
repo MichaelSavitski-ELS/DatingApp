bea41745425cd5faeb8f761c08a234814e964bed
Savitski <SAVITSKIM@science.regn.net>
2020-07-21 13:14:45 -0400

    Lessons 139-142

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DatingApp.API.Helpers;
 using DatingApp.API.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -45,6 +46,13 @@ namespace DatingApp.API.Data
         {
             var users = await _context.Users.Include(p => p.Photos).ToListAsync();
             return users;
+        }
+
+        public async Task<PagedList<User>> GetUsers(UserParams userParams)
+        {
+            var users = _context.Users.Include(p => p.Photos);
+
+            return await PagedList<User>.CreateAsync(users, userParams.PageNumber, userParams.PageSize);
         }
 
         public async Task<bool> SaveAll()
